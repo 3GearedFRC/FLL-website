d9989679563ea52c52373278213cf6b16c93b9e3
unknown <hec.earth.version.c.137@gmail.com>
2020-01-28 00:55:06 +0100

    logo 2 pick

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ window.addEventListener("scroll", function() {
   const parallax = document.querySelector(".parallax");
   let scrollPosition = window.pageYOffset;
   parallax.style.transform = "translateY(" + scrollPosition * -0.5 + "px)";
+  this.console.log(scrollPosition * -0.5 );
 });
